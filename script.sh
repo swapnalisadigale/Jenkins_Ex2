2e5788739ef5c60b2b7f29f61d5aa413c62be599
swapnalisadigale <44576145+swapnalisadigale@users.noreply.github.com>
2019-01-24 12:11:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 /usr/bin/arm-linux-gnueabi-g++ -o serverdum server.c
 sshpass -p priyanka@26 scp serverdum pi@10.110.7.66:/home/pi
-echo RUNNING ON $PROC $NODE
+echo RUNNING ON Processor:$PROC Node:$NODE
