5794acadb40db4f63653f2452ae4aab184078603
swapnalisadigale <44576145+swapnalisadigale@users.noreply.github.com>
2019-01-24 11:58:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
 /usr/bin/arm-linux-gnueabi-g++ -o ser11 server.c
-sshpass -p \'priyanka@26\' scp ser11 pi@10.110.7.66:/home/pi
+sshpass -p priyanka@26 scp ser11 pi@10.110.7.66:/home/pi
+ECHO RUNNING ON $PROC $NODE
